2adf9cc0a09ceed5fb9867f45e37d140849409d6
rafael eduardo <rafaelMejia@rafaels-MacBook-Pro-2.local>
2021-05-22 13:55:15 -0600

    “Timeline-GSAP”

--- src/router.js
+++ src/router.js
@@ -51,6 +51,12 @@ export default new Router({
       name: "state",
       component: () =>
         import(/* webpackChunkName: "list" */ './views/State.vue')
+    },
+    {
+      path: "/timeline",
+      name: "timeline",
+      component: () =>
+        import(/* webpackChunkName: "list" */ './views/Timeline.vue')
     }
   ]
 });
