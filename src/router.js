c1e74d59b3748003cab7ce94de45e02cca6d12b1
rafael eduardo <rafaelMejia@rafaels-MacBook-Pro-2.local>
2021-05-23 13:49:27 -0600

    “Timeline-Master”

--- src/router.js
+++ src/router.js
@@ -57,6 +57,12 @@ export default new Router({
       name: "timeline",
       component: () =>
         import(/* webpackChunkName: "list" */ './views/Timeline.vue')
+    },
+    {
+      path: "/master",
+      name: "master",
+      component: () =>
+        import(/* webpackChunkName: "list" */ './views/Master.vue')
     }
   ]
 });
